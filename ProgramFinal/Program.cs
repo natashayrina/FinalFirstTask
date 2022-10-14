391ce2c5ba78fc3e2d65e10e58e12290de4c83c9
Natalia Shaurina <natashayrina@mail.ru>
2022-10-14 13:20:20 +0300

    Итоговая задача 1 четверть.

---
+++ ProgramFinal/Program.cs
@@ -0,0 +1,34 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+// Первоначально массив можно ввсети с клавиатуры, либо задать на старте выполненния алгоритма. 
+//Пример ( "hello", "2", "world" --- ("2", ";").
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = new string[5] {"hello", "2", "world", "10", ";"};
+string [] result = new string [array.Length];
+
+int pos = 0;
+int len = 3;
+
+for (int j=0; j < array.Length; j++)
+      
+   {
+      if (array[j].Length <= len)
+      {
+        result[pos] = array [j]; 
+        pos++;
+      } 
+   }  
+
+Console.WriteLine();
+PrintArray (array);
+Console.WriteLine();
+PrintArray (result);
+   
